c68776a1c69ed2ca32de952c292fbef2c87e46dd
Arash Homayouni <ahomayouni@hoopp.com>
2020-06-25 10:10:11 -0400

    sample react redux test map state to props in questionDetail.js

--- src/components/__tests__/QuestionDetails.js
+++ src/components/__tests__/QuestionDetails.js
@@ -1,5 +1,22 @@
+import { mapStateToProps } from "../QuestionDetail";
+// Map state to props sample
 describe(`The Question Detail Component`, () => {
-  it(`should not regress`, () => {
-    expect(2 + 2).toEqual(4);
+  describe(`The Container Element`, () => {
+    describe("mapStateToPros", () => {
+      it("should map the state to props correctly", () => {
+        const sampleQuestion = {
+          question_id: 42,
+          body: "Something",
+        };
+        const appState = {
+          questions: [sampleQuestion],
+        };
+        const ownProps = {
+          question_id: 42,
+        };
+        const componentState = mapStateToProps(appState, ownProps);
+        expect(componentState).toEqual(sampleQuestion);
+      });
+    });
   });
 });
